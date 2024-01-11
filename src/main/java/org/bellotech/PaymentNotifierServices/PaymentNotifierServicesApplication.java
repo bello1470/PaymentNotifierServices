babbbc2855e018ad575b74681a2142d75c8a12e1
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-11 07:15:02 +0000

    first commit

---
+++ src/main/java/org/bellotech/PaymentNotifierServices/PaymentNotifierServicesApplication.java
@@ -0,0 +1,13 @@
+package org.bellotech.PaymentNotifierServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentNotifierServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentNotifierServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bellotech/PaymentNotifierServices/PaymentNotifierServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bellotech.PaymentNotifierServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentNotifierServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
