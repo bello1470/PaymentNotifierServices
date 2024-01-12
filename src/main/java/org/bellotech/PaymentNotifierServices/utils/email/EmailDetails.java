1f688d3f39b9800806f17cc8998d638c1c95116c
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-12 07:22:42 +0000

    Emailservices refactor commits

--- src/main/java/org/bellotech/PaymentNotifierServices/utils/email/EmailDetails.java
+++ src/main/java/org/bellotech/PaymentNotifierServices/utils/email/EmailDetails.java
@@ -1,8 +1,21 @@
 package org.bellotech.PaymentNotifierServices.utils.email;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailDetails {
-    private String recipent;
-    private String msgBody;
-    private String subject;
+    @Schema(name = "recipent email", description = "abubakarbello@264",example = "abubakarbello")
+    private String recipent = "abubakarbello@264";
+    @Schema(name = "Body",description="payment received", example="Body")
+    private String msgBody="payment received";
+    @Schema(name = "sublect", description = "payment comfirmation", example = "payment comfirmation" )
+    private String subject  = "payment comfirmation";
 
 }
