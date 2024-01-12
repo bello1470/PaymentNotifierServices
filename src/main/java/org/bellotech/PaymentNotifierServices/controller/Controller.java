4e5a87fe3d3e6a50ab9fcbd258c8c0ab80d6c639
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-12 10:50:45 +0000

    controller commit

--- src/main/java/org/bellotech/PaymentNotifierServices/controller/Controller.java
+++ src/main/java/org/bellotech/PaymentNotifierServices/controller/Controller.java
@@ -3,11 +3,9 @@ package org.bellotech.PaymentNotifierServices.controller;
 import org.bellotech.PaymentNotifierServices.services.EmailService;
 import org.bellotech.PaymentNotifierServices.utils.email.EmailDetails;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -19,14 +17,13 @@ public class Controller {
     @Autowired
     private EmailService emailService;
 
-    @Autowired
-    private JavaMailSender javaMailSender;
+  
 
-    @PostMapping("/send")
-    public String sendEmail(@RequestBody EmailDetails emailDetails ) {
-        String message = "Sent";
-        EmailDetails emailDetails2 = new EmailDetails("abubakarmustapha264@outlook.com",message,"payment successfull");
-        emailService.sendSimpleEmail(emailDetails2);
+    @PostMapping("/send/{mail}")
+    public String sendEmail(@PathVariable String mail, @RequestBody EmailDetails emailDetails ) {
+      
+      
+        emailService.sendSimpleEmail(mail,emailDetails);
         return "Email sent successfully!";
     }
     
