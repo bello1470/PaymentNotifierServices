d88cc116b52c4f5be28cf876b8c07259016ab1c3
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-12 08:22:51 +0000

    controller refactor commits

--- src/main/java/org/bellotech/PaymentNotifierServices/controller/Controller.java
+++ src/main/java/org/bellotech/PaymentNotifierServices/controller/Controller.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
+@RequestMapping("/mail")
 public class Controller {
 
     @Autowired
@@ -20,10 +22,10 @@ public class Controller {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    @PostMapping("/")
+    @PostMapping("/send")
     public String sendEmail(@RequestBody EmailDetails emailDetails ) {
         String message = "Sent";
-        EmailDetails emailDetails2 = new EmailDetails("abubakarbello264@gmail.com",message,"payment successfull");
+        EmailDetails emailDetails2 = new EmailDetails("abubakarmustapha264@outlook.com",message,"payment successfull");
         emailService.sendSimpleEmail(emailDetails2);
         return "Email sent successfully!";
     }
