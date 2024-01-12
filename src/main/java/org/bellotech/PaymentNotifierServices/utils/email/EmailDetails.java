37b97ce41fff1f3ed54167bfe4e77b311b6f7f92
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-12 06:00:27 +0000

    EmailDetails commits

---
+++ src/main/java/org/bellotech/PaymentNotifierServices/utils/email/EmailDetails.java
@@ -0,0 +1,8 @@
+package org.bellotech.PaymentNotifierServices.utils.email;
+
+public class EmailDetails {
+    private String recipent;
+    private String msgBody;
+    private String subject;
+
+}
