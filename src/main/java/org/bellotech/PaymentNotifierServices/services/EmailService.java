71a9d16dda569b5fdb4d8c803327a1e6926aaf81
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-12 10:50:58 +0000

    emailservices commits

--- src/main/java/org/bellotech/PaymentNotifierServices/services/EmailService.java
+++ src/main/java/org/bellotech/PaymentNotifierServices/services/EmailService.java
@@ -6,7 +6,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import lombok.Value;
+
 
 @Service
 public class EmailService {
@@ -17,13 +17,13 @@ public class EmailService {
     @org.springframework.beans.factory.annotation.Value("${spring.mail.username}")
     private String sender;
 
-    public Boolean sendSimpleEmail(EmailDetails emailDetails){
+    public Boolean sendSimpleEmail(String mail, EmailDetails emailDetails){
 
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
             
             mailMessage.setFrom(sender);
-            mailMessage.setTo(emailDetails.getRecipent());
+            mailMessage.setTo(mail);
             mailMessage.setText(emailDetails.getMsgBody());
             mailMessage.setSubject(emailDetails.getSubject());
 
