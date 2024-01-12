bbeb5221b500427c387d86d1b7df283f348586d6
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-12 10:51:10 +0000

    emaildetails comits

--- src/main/java/org/bellotech/PaymentNotifierServices/utils/email/EmailDetails.java
+++ src/main/java/org/bellotech/PaymentNotifierServices/utils/email/EmailDetails.java
@@ -11,9 +11,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailDetails {
-    @Schema(name = "recipent email", description = "abubakarbello@264",example = "abubakarbello")
-    private String recipent = "abubakarbello@264";
-    @Schema(name = "Body",description="payment received", example="Body")
+    @Schema(name = "recipent", description = "abubakarmustapha264@outlook.com",example = "abubakarmustapha264@outlook.com")
+    private String recipent = "abubakarmustapha264@outlook.com";
+    @Schema(name = "msgBody",description="payment received", example="Body")
     private String msgBody="payment received";
     @Schema(name = "sublect", description = "payment comfirmation", example = "payment comfirmation" )
     private String subject  = "payment comfirmation";
