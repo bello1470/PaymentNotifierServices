7a93cf2ca4f5c5421cc64177d6178be5ba0e76f3
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-12 07:22:12 +0000

    Appconfig refactor commit

--- src/main/java/org/bellotech/PaymentNotifierServices/config/AppConfig.java
+++ src/main/java/org/bellotech/PaymentNotifierServices/config/AppConfig.java
@@ -1,5 +1,7 @@
 package org.bellotech.PaymentNotifierServices.config;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,17 +11,17 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class AppConfig {
 
-    @Value("${pring.mail.host}")
+    @Value("${spring.mail.host}")
     private String mail_host;
     @Value("${spring.mail.port}")
-    private int mail_port;
+    private String mail_port;
 
     
     @Value("${spring.mail.properties.mail.smtp.auth}")
     private String mail_properties_mail_smtp_auth;
 
     @Value("${spring.mail.properties.mail.smtp.startls.enable}")
-    private String _mai_properties_mail_smtp_startls_enable;
+    private String mai_properties_mail_smtp_startls_enable;
 
     @Value("${spring.mail.smtp.ssl.trust}")
     private String smtp_ssl_trust;
@@ -27,7 +29,7 @@ public class AppConfig {
     @Value("${mail.transport.protocol}")
     private String mail_transport_Protocol;
 
-    @Value("${Spring.mail.username}")
+    @Value("${spring.mail.username}")
     private String spring_mail_username;
 
     @Value("${spring.mail.password}")
@@ -37,6 +39,21 @@ public class AppConfig {
     public JavaMailSender javaMailSender(){
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(mail_host);
+        mailSender.setPort(Integer.parseInt(mail_port));
+        mailSender.setUsername(spring_mail_username);
+        mailSender.setPassword(sprin_mail_passwrod);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", mail_transport_Protocol);
+        props.put("mail.smtp.auth", mail_properties_mail_smtp_auth);
+        props.put("mail.smtp.Starttls.enable", mai_properties_mail_smtp_startls_enable);
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.ssl.trust", smtp_ssl_trust );
+
+        return mailSender;
+
     }
     
 }
